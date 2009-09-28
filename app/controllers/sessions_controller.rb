41bddcc3b00965f9fd7b09a10a446baa86284051
Jason Oglesby <jason@youdel.com>
2009-09-28 01:23:14 -0500

    commit message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -44,8 +44,8 @@ class SessionsController < ApplicationController
     flash.now[:error] = message
     respond_to do |format|
       format.html { render :action => 'new' }
-      format.xml { render :xml => message, :status => :unprocessable_entity }
-      format.json  { render :json => message, :status => :unprocessable_entity }
+      format.xml { render :xml => :error, :status => :unprocessable_entity }
+      format.json  { render :json => :error, :status => :unprocessable_entity }
     end
   end
  
