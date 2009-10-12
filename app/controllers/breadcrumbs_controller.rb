29a26305a619d1f5dc66161666b7fc71195d1f37
Jason Oglesby <jason@youdel.com>
2009-10-12 14:37:32 -0500

    commit message

---
+++ app/controllers/breadcrumbs_controller.rb
@@ -0,0 +1,22 @@
+class BreadcrumbsController < ApplicationController
+layout 'application'
+before_filter :login_required
+
+  def create
+    @breadcrumb = Breadcrumb.new(params[:breadcrumb])
+    @breadcrumb.user = current_user
+
+    respond_to do |format|
+      if @breadcrumb.save
+        flash[:notice] = 'Breadcrumb was successfully created.'
+        format.xml  { render :xml => @breadcrumb, :status => :created, :location => @breadcrumb }
+      else
+        format.xml  { render :xml => @breadcrumb.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def index
+    @breadcrumbs = Breadcrumb.find(:all)
+  end
+end
